c0f9ab7e3b7209978dfbcfdaaceb2ce729244d63
Vaimer9 <vaim3r9@gmail.com>
2021-11-22 11:17:36 +0530

    init

---
+++ src/utils/args.rs
@@ -0,0 +1,34 @@
+use structopt::StructOpt;
+use std::path::PathBuf;
+
+#[derive(StructOpt, Debug)]
+#[derive(name = "crpc")]
+pub enum Args {
+    set {
+        #[structopt(short = "s")]
+        status: String,
+
+        #[structopt(short = "d")]
+        details: String,
+
+        #[structopt(short = "li")]
+        large_image: String,
+
+        #[structopt(short = "si")]
+        small_image: String,
+        
+        #[structopt(short = "lit")]
+        large_image_text: String,
+
+        #[structopt(short = "sit")]
+        small_image_text: String,
+
+    },
+
+    set_dir {
+        #[structopt(short = "f")]
+        file: PathBuf
+    },
+
+    set_conf 
+}
